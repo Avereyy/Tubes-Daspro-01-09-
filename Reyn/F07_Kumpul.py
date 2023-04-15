139d2d0a0786dff25e04dc9ac3650bdf2c2d92b4
Earbudgit <130482572+Earbudgit@users.noreply.github.com>
2023-04-16 03:42:43 +0700

    Update F07_Kumpul.py

--- Reyn/F07_Kumpul.py
+++ Reyn/F07_Kumpul.py
@@ -4,7 +4,9 @@ import random
 nama = newlist(nama)
 deskripsi = newlist(deskripsi)
 jumlah = newlist_int(jumlah)
+sisa_candi = 100
 
+#Fungsi F07: mengumpulkan bahan
 def kumpul():
     pasir = random.randint(0, 5)
     batu = random.randint(0, 5)
@@ -18,3 +20,31 @@ def kumpul():
     edit("bahan_bangunan.csv", ["air","dari laut",str(add_air)],3)
 
     print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
+    
+#Fungsi F06: membangun candi
+def bahancukup(needed_pasir,needed_batu,needed_air):
+    if (jumlah[0] >= needed_pasir) and (jumlah[1] >= needed_batu) and (jumlah[2] >= needed_air):
+        return True
+    else:
+        return False
+
+def bangun():
+    needed_pasir = random.randint(0, 5)
+    needed_batu = random.randint(0, 5)
+    needed_air = random.randint(0, 5)
+    print(f"pasir: {needed_pasir} batu: {needed_batu} air: {needed_air}")
+    if bahancukup(needed_pasir,needed_batu,needed_air):
+        pakai_pasir = jumlah[0] - needed_pasir
+        pakai_batu = jumlah[1] - needed_batu
+        pakai_air = jumlah[2] - needed_air
+
+        edit("bahan_bangunan.csv", ["Pasir","dari pantai",str(pakai_pasir)],1)
+        edit("bahan_bangunan.csv", ["Batu","dari gunung",str(pakai_batu)],2)
+        edit("bahan_bangunan.csv", ["air","dari laut",str(pakai_air)],3)
+
+        global sisa_candi 
+        sisa_candi = sisa_candi - 1
+
+        print("Candi berhasil dibangun." + f"\nSisa candi yang perlu dibangun: {sisa_candi}.")
+    else:
+        print("Bahan bangunan tidak mencukupi.")
